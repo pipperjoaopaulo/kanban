b0d7ca1a54deb15631f6dd9a24fe4ec9e0a73028
pipperjoaopaulo <pipperjoaopaulo@gmail.com>
2026-08-28 10:10:13 -0300

    recomeço

---
+++ app/src/main/java/com/joaopaulo/kanban/ui/SplashFragment.kt
@@ -0,0 +1,30 @@
+package com.joaopaulo.kanban.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.joaopaulo.kanban.R
+import com.joaopaulo.kanban.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
+    }
+
+}
